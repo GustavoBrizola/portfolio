4aaa5380f0b1793b746ff73961685f2a6580f227
GustavoBrizola <gustavobrizola53@gmail.com>
2026-09-02 10:28:17 -0300

    Testing deployment of separate branches #3

---
+++ src/jsx/portfolio/ContentDisplay.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+export
+const SectionDisplay = ({PageMap, SetActiveSection}) => (
+    <>
+        {Object.entries(PageMap).slice(0, 6).map(([key, value]) => (
+            <a className = 'nav-link'key={key} href={value.label} onClick={(e) => { e.preventDefault(); SetActiveSection(key);}}>
+                {value.icon && <img src={value.icon} alt={null} style={{width: '18px'}}/>}
+                {value.label && <span>{value.label}</span>}
+            </a>
+        ))}
+    </>
+)
+
+
+export
+const ContentDisplay = ({PageMap, ActiveSection }) => {
+    return (<>{PageMap[ActiveSection]?.component || <></>}</>)
+}
+
