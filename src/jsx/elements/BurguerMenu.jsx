0a1d365589ced251b4433c815dccb19bdb4d4392
GustavoBrizola <gustavobrizola53@gmail.com>
2026-09-02 10:20:38 -0300

    Testing deployment of separate branches

--- src/jsx/elements/BurguerMenu.jsx
+++ src/jsx/elements/BurguerMenu.jsx
@@ -9,7 +9,7 @@ function BurguerMenu(props) {
     return (
         <div>
             <div style={{height:'100%', width:'100%', left:'0', top:'0', position:'fixed', display:'flex', backgroundColor:'black', zIndex:'999', opacity:'0.7'}}/>
-            <nav style={{display:'flex', position:"fixed", height: '50%', width:'100%', zIndex:'999', left:'0', backgroundColor: '#272433', borderBottom: '3px solid #5B5576', opacity:'1'}}>
+            <nav style={{display:'flex', position:"fixed", height: '50%', width:'100%', zIndex:'999', left:'0', backgroundColor: '#363245', borderBottom: '3px solid #5B5576', opacity:'1'}}>
             </nav>
         </div>
     )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,32 +2,30 @@ import React, {useState, useEffect} from "react";
 
 import'./Portfolio.css';
 
+import { pageMap } from "jsx/portfolio/SectionList";
+
 import burguerIcon from '/svg/ui/lines_white.svg';
 import BurguerMenu from "jsx/elements/BurguerMenu";
 
-import { pageMap } from "jsx/portfolio/SectionList";
 import { DevCredits, BuildVersion, FooterLinks } from "jsx/portfolio/Footer";
 import { ProfileDisplay } from "jsx/portfolio/NavigationBar";
+import { SectionDisplay, ContentDisplay } from "jsx/portfolio/ContentDisplay";
 
 export default
 function Portfolio() {
     const [activeSection, setActiveSection] = useState(pageMap.home.id);
     const [isBurguerMenu, setBurguerMenu] = useState(false);
-    let SectionNumber = Math.floor(window.innerWidth/200);
 
     return (
         <main className="portfolio">
-            {isBurguerMenu && <BurguerMenu/>}
 
+            {isBurguerMenu && <BurguerMenu/>}
             <nav className="navigationBar">
                 <ProfileDisplay/>
                 <div className='section'>
-                    {Object.entries(pageMap).slice(0, SectionNumber).map(([key, value]) => (
-                        <a className = 'nav-link'key={key} href={value.label} onClick={(e) => { e.preventDefault(); setActiveSection(key);}}>
-                            {value.icon && <img src={value.icon} alt={null} style={{width: '18px'}}/>}
-                            {value.label && <span>{value.label}</span>}
-                        </a>
-                    ))}
+
+                    {/* Temporary */}
+                    <SectionDisplay SetActiveSection={setActiveSection} PageMap={pageMap}/>
                     <a onClick={() => setBurguerMenu(!isBurguerMenu)}>
                         <img src={burguerIcon} alt={null} style={{width: '18px'}}/>
                     </a>
@@ -36,7 +34,10 @@ function Portfolio() {
 
             <section className="contentDisplay">
                 <div className='content'>
-                    {pageMap[activeSection]?.component || <></>}
+
+                    {/* Temporary */}
+                    <ContentDisplay ActiveSection={activeSection} PageMap={pageMap}/>
+
                 </div>
             </section>
 
